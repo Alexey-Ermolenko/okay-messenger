d25e74201dab2159830de428db8183828d16d534
Ermolenko <a.o.ermolenko@gmail.com>
2025-03-03 18:49:50 +0200

    fixed code-style controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,7 +13,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/v1', name: 'api_index')]
+#[Route(
+    path: '/api/v1',
+    name: 'api_index',
+)]
 final class IndexController extends AbstractController
 {
     public const SUCCESS_BODY = ['result' => 'success'];
@@ -23,25 +26,39 @@ final class IndexController extends AbstractController
     ) {
     }
 
-    #[Route('/healthcheck/ping', name: 'healthcheck_ping', methods: [Request::METHOD_GET])]
+    #[Route(
+        path: '/healthcheck/ping',
+        name: 'healthcheck_ping',
+        methods: [Request::METHOD_GET],
+    )]
     public function ping(): JsonResponse
     {
         return new JsonResponse(self::SUCCESS_BODY, Response::HTTP_OK);
     }
 
-    #[Route('/accept/user/{user_id}/friend/{friend_id}', name: 'api_accept_user_friend', methods: [Request::METHOD_GET])]
-    public function acceptFriend(int $user_id, int $friend_id): Response
+    #[Route(
+        path: '/accept/user/{user_id}/friend/{friend_id}',
+        name: 'api_accept_user_friend',
+        methods: [Request::METHOD_GET],
+    )]
+    public function acceptFriend(int $user_id, int $friend_id): JsonResponse
     {
         $result = $this->userService->acceptFriendRequest($user_id, $friend_id);
-        return $this->json($result);
+        return new JsonResponse($result, Response::HTTP_OK);
     }
 
-    #[Route('/notification-preferences-list/get', name: 'api_accept_user_friend', methods: [Request::METHOD_GET])]
-    public function getNotificationPreferencesList(Request $request): JsonResponse
+    #[Route(
+        path: '/notification-preferences-list/get',
+        name: 'api_accept_user_friend',
+        methods: [Request::METHOD_GET],
+    )]
+    public function getNotificationPreferencesList(): JsonResponse
     {
-        return $this->json([
+        return new JsonResponse([
             'result' => RequestStatus::Success,
             'notification_preferences' => NotificationPreference::cases(),
-        ]);
+        ],
+            Response::HTTP_OK,
+        );
     }
 }
